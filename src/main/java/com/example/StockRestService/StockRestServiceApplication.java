3a66c4218f01368c74a18871482b184b0961947d
advarice <chen.yaofeng@yahoo.com>
2019-02-02 11:37:56 -0500

    initial commit

---
+++ src/main/java/com/example/StockRestService/StockRestServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.StockRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockRestServiceApplication.class, args);
+	}
+
+}
+
